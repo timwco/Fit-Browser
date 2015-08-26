f1d89722d9325c7430bd430046aba45a02c301ff
Tim Whitacre <tim@timw.co>
2015-08-26 14:45:03 -0400

    Manipulate all the data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,13 +8,27 @@
 
     $http.get(PARSE.URL + 'classes/entry?include=excercise', PARSE.CONFIG)
       .success( function (res) {
-        var data = res.results;
 
-        var excercises = _.uniq(_.map(data, function (d) {
+        // Store our Data
+        var entries = res.results;
+
+        // Pluck out all of the Excercise Names
+        var excercises = _.uniq(_.map(entries, function (d) {
           return d.excercise.name;
         }));
 
-        console.log(excercises);
+        // Filter our items to group together similiar excercises
+        var filteredEntries = _.map(excercises, function (exName) {
+          return _.filter(entries, function (obj) {
+            return obj.excercise.name === exName;
+          });
+        });
+
+        // Create an array of our sorted entires by name
+        // Bind to $scope
+        $scope.sortedEntries = _.sortBy(filteredEntries, function (e) {
+          return e[0].excercise.order;
+        });
 
       });
 
